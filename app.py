1d14c8a1ba91da2603e2e4afb12addff3d7b273a
Renan Teixeira <renan.silva@ng7consultoria.com.br>
2025-02-04 20:42:55 -0300

    adicionado o template de login e também a funcionalidade

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+
 
 app = Flask(__name__)
 
+
+app.config['SECRET_KEY'] = secrets.token_hex(8)
 #O ENDEREÇO DO BANCO
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://deployApiDB_chosenmove:7ab603edd69d6ff610c42e6579f19dfb4f0280a9@2zrxo.h.filess.io:5433/deployApiDB_chosenmove?options=-csearch_path%3Ddbo,public,meu_schema'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -53,6 +57,34 @@ def criar_usuario():
         return "Usuário criado com sucesso!", 201
     else:
         return render_template('cadastro.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+
+        # Verificar se os campos estão preenchidos
+        if not nome or not senha:
+            flash('Nome e senha são obrigatórios!', 'error')
+            return redirect(url_for('login'))
+
+        # Buscar o usuário no banco de dados
+        usuario = Usuario.query.filter_by(nome=nome).first()
+
+        # Verificar se o usuário existe e a senha está correta
+        if usuario and usuario.verificar_senha(senha):
+            flash('Login bem-sucedido!', 'success')
+            return redirect(url_for('home'))  # Roteamento para uma página inicial (home)
+        else:
+            flash('Credenciais inválidas', 'error')
+            return redirect(url_for('login'))
+
+    # Se o método for GET, exibe o formulário de login
+    return render_template('login.html')
+
+
 
 
 
